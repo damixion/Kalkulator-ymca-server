0e7e316f711378ff175f026b7016072ec311fd8d
Damian <55909356+damixion@users.noreply.github.com>
2021-12-11 16:13:05 +0100

    Update MeasurementService.java

--- src/main/java/com/ymca/ymca/service/MeasurementService.java
+++ src/main/java/com/ymca/ymca/service/MeasurementService.java
@@ -45,11 +45,6 @@ public class MeasurementService {
           return listEqualMeasurement;
     }
 
-    @EventListener(ApplicationReadyEvent.class)
-    public void addProductForDb()
-    {
-
-    }
 }
 
 
